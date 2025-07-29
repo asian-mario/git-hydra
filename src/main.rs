d90ac6346d23586b273a65d639863f5f4e07c73f
asian-mario <ranggagunarto@gmail.com>
2025-07-29 14:00:40 +0800

    Initial Setup

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+use clap::{Paerser, Subcommand};
+use std::path::PathBuf;
+
+mod app;
+mod git;
+mod ui;
+
+use app::App;
+
+#[derive(Parser)]
+#[command(name = "git-hydra")]
+#[command(about = "a tui git interface inspired by GitKraken.")]
+
+struct Cli{
+    // repo path (defaults to the current dir.)
+    #[arg(short, long)]
+    repo: Option<PathBuf>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Ui,
+    Status,
+    Log {
+        #[arg(short, long, default_value = "10")]
+        count: usize,
+    }
+}
